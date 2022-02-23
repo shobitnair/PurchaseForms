691e662aca7653679fa4f89bcebba2ea04164cbf
shobit <shobitnair10@gmail.com>
2022-02-23 21:17:04 +0530

    upd

--- client/src/cred.js
+++ client/src/cred.js
@@ -1,1 +1,2 @@
-export const URL = 'http://34.100.213.133';
+//export const URL = 'http://34.100.213.133';
+export const URL = 'http://localhost:8000'
--- server/db.js
+++ server/db.js
@@ -3,7 +3,8 @@ const Pool = require('pg').Pool;
 const pool  = new Pool({
     user:"postgres",
     password:"Vrindavan123",
-    host:"34.100.245.249",
+    host : "localhost",
+   // host:"34.100.245.249",
     port:"5432",
     database:"demo"
 });
