067a2032e5c576035b0fbb423a5142ec54bcc527
shobit <shobitnair10@gmail.com>
2022-02-24 21:28:28 +0530

    'fixes'

--- client/src/Login/LoginCheck.js
+++ client/src/Login/LoginCheck.js
@@ -3,17 +3,17 @@ import { auth, provider } from "../FireBase";
 import {setUser , setLogin} from "../Store/actions"
 
 
-export function LoginCheck(state , dispatch){
+export function LoginCheck(dispatch){
     auth.onAuthStateChanged(async (authUser) => {
         if (authUser) {
             dispatch(setUser({
                 uid: authUser.uid,
                 photo: authUser.photoURL,
                 email: authUser.email,
-                displayName: authUser.displayName,
             }));
+            console.log(authUser.displayName)
         } else {
             dispatch(setUser(null));
         }
-    });
+    })
 }
