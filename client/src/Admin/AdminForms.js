e70141d3f29c26c68995fa8788d0c3b77f9c1482
shobitnair <shobitnair10@gmail.com>
2022-03-15 20:26:22 +0530

    'fixes'

--- client/src/Admin/AdminForms.js
+++ client/src/Admin/AdminForms.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {
     ConstrainMode,
     Depths,
-    DetailsList, DetailsListLayoutMode, Label, PrimaryButton, Selection, SelectionMode, TextField
+    DetailsList, DetailsListLayoutMode, Dropdown, Label, PrimaryButton, Selection, SelectionMode, TextField
 } from '@fluentui/react'
 import {URL} from '../cred'
 import axios from "axios";
@@ -105,11 +105,30 @@ const _columns = [
     },
 ];
 
+const formOptions = [
+    { key: 'A', text: 'All'},
+    { key: 'B', text: 'Sp101' },
+    { key: 'C', text: 'Sp102' },
+];
+
+const statusOptions = [
+    { key: 'A', text: 'All'},
+    { key: 'B', text: 'Pending' },
+    { key: 'C', text: 'Approved' },
+    { key: 'C', text: 'Denied' },
+]
+
 const AdminForms = () => {
     const [items , setItems] = useState([])
     const [forms , setForms] = useState([])
     const [temp , setTemp] = useState([])
-    const [search , setSearch] = useState('')
+    const [first , setFirst] = useState(true)
+    const [filter , setFilter] = useState({
+        id:'',
+        email:'',
+        type:'' ,
+        status:'',
+    })
 
 
     const getForms = async() =>{
@@ -142,31 +161,56 @@ const AdminForms = () => {
 
     }
 
-    const handleSearch = (search) =>{
+    const exists = (a , b) => {
+        return a.toString().toLowerCase().indexOf(b.toString().toLowerCase()) > -1
+    }
+
+    const updateForms = () =>{
         setItems(temp.filter(x =>{
-            if(x.email.toString().toLowerCase().indexOf(search.toLowerCase()) > -1)return 1
-            if(x.id.toString().toLowerCase().indexOf(search.toLowerCase()) > -1)return 1
-            if(x.status.toString().toLowerCase().indexOf(search.toLowerCase()) > -1)return 1
-            if(x.type.toString().toLowerCase().indexOf(search.toLowerCase()) > -1)return 1
+            if(
+                exists(x.email , filter.email) &&
+                exists(x.id , filter.id) &&
+                (filter.type === 'All' || exists(x.type , filter.type)) &&
+                (filter.status === 'All' || exists(x.status , filter.status))
+            ) return 1
+            else return 0
         }))
-        setSearch(search)
     }
 
     useEffect(async () => {
-        await getForms()
-    } , [])
+        if(first){
+            await getForms()
+            setFirst(false)
+        } else {
+            updateForms()
+        }
+    } , [first , filter])
 
     const [selection, setSelection] = useState(new Selection());
     return (<>
-        <Grid templateColumns='repeat(12,1fr)' templateRows='repeat(12,1fr)' w='100%'  h='600px' gap={1} m={4}>
-            <GridItem rowSpan={1} colSpan={2}>
+        <Grid templateColumns='repeat(12,1fr)' templateRows='repeat(12,1fr)' w='100%'  h='600px' gap={4} >
+            <GridItem rowSpan={1} colSpan={2} ml={4}>
                 <TextField
-                    label = "Search"
-                    onChange={(e)=>handleSearch(e.target.value)}
+                    label = "Filter by ID"
+                    onChange={(e)=>setFilter({...filter,id:String(e.target.value)})}
                     placeholder={'Enter any keyword'}></TextField>
             </GridItem>
+            <GridItem  rowStart={2} rowSpan={1} colSpan={2} ml={4}>
+                <TextField
+                    label = "Filter by Email"
+                    onChange={(e)=>setFilter({...filter , email:e.target.value})}
+                    placeholder={'Enter any keyword'}></TextField>
+            </GridItem>
+            <GridItem rowSpan={1} colSpan={2} ml={4}>
+                <Dropdown placeholder="Select an Option" options={formOptions} label="Form type"
+                          onChange={(e, i) => setFilter({ ...filter, type: i.text })} />
+            </GridItem>
+            <GridItem  rowStart={2} rowSpan={1} colSpan={2} ml={4}>
+                <Dropdown placeholder="Select an Option" options={statusOptions} label="Status type"
+                          onChange={(e, i) => setFilter({ ...filter, status: i.text })} />
+            </GridItem>
 
-            <GridItem rowStart={3} colSpan={12} m={8}>
+            <GridItem rowStart={3} rowSpan={10} colSpan={12} m={8}>
                 <div style={{'border': '8px solid #f3f2f1' , borderRadius: '2px', boxShadow: Depths.depth4 }}>
                     <DetailsList
                         items={items}
