02acf98cf17f0b24030e5db38df2827fdfe8b151
shobitnair <shobitnair10@gmail.com>
2022-03-18 17:19:58 +0530

    'fixes'

---
+++ demo_public_users.sql
@@ -0,0 +1,14 @@
+create table users
+(
+    name  varchar not null,
+    email varchar not null
+        primary key,
+    role  varchar
+);
+
+alter table users
+    owner to postgres;
+
+INSERT INTO public.users (name, email, role) VALUES ('Sujaya prashant', 'sujaprasha1995@gmail.com', 'FACULTY');
+INSERT INTO public.users (name, email, role) VALUES ('Shobit Nair', 'shobitnair10@gmail.com', 'FACULTY');
+INSERT INTO public.users (name, email, role) VALUES ('SHOBIT PRASHANT NAIR', '2019csb1121@iitrpr.ac.in', 'JAO');
