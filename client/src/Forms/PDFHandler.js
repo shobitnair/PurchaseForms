9b23f20f49c5f34aa7d110c79b0be8f2a1b47c83
shobitnair <shobitnair10@gmail.com>
2022-03-26 00:55:38 +0530

    'fixes'

--- client/src/Forms/PDFHandler.js
+++ client/src/Forms/PDFHandler.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {PDFsp101} from "./PDFsp101";
-import { getFormById } from '../Requests/formRequests';
+import { getDraftById, getFormById } from '../Requests/formRequests';
 
 export const PDFHandler = (form , data) =>{
     if(form === 'sp101')PDFsp101(data)
@@ -8,5 +8,10 @@ export const PDFHandler = (form , data) =>{
 
 export const PDFbyID = async (id) => {
     const response = await getFormById(id)
+    PDFHandler(response.type , JSON.parse(response.data))
+}
+
+export const DraftByID = async(id , email) =>{
+    const response = await getDraftById(id , email)
     PDFHandler(response.type , JSON.parse(response.data))
 }
