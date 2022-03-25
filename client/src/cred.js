d7cc6b2bbbb70bcfb331fc3d419991ef01055382
shobitnair <shobitnair10@gmail.com>
2022-03-26 00:03:52 +0530

    'fixes'

--- client/src/cred.js
+++ client/src/cred.js
@@ -1,2 +1,6 @@
-export const URL = 'http://localhost:8000/api'
+//export const URL = 'http://65.0.124.177';
+
+export const URL = process.env.NODE_ENV === 'production'
+    ?'/api'
+    :'http://localhost:8000/api'
       
