1fdc9d11f150aa21f3a46c32259e452f3c995efd
shobitnair <shobitnair10@gmail.com>
2022-04-03 15:40:03 +0530

    'fixes'

--- client/src/cred.js
+++ client/src/cred.js
@@ -1,4 +1,4 @@
 const AWS = 'http://65.0.131.63/api'
-const AZURE = 'http://pf-iitrpr.eastasia.cloudapp.azure.com/site'
+const AZURE = 'http://pf-iitrpr.eastasia.cloudapp.azure.com/api'
 const local = 'http://localhost:8000/api'
 export const URL = AZURE
