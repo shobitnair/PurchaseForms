38199f872f44189959a777280afb793f00ed42b2
shobitnair <shobitnair10@gmail.com>
2022-04-30 01:32:03 +0530

    'fixes'

--- demo_public_users.sql
+++ demo_public_users.sql
@@ -1,16 +1,17 @@
 create table users
 (
-    name  varchar not null,
-    email varchar not null
+    name       varchar not null,
+    email      varchar not null
         primary key,
-    role  varchar
+    role       varchar,
+    department varchar
 );
 
 alter table users
     owner to postgres;
 
-INSERT INTO public.users (name, email, role) VALUES ('Sujaya prashant', 'sujaprasha1995@gmail.com', 'FACULTY');
-INSERT INTO public.users (name, email, role) VALUES ('Shobit Nair', 'shobitnair10@gmail.com', 'FACULTY');
-INSERT INTO public.users (name, email, role) VALUES ('SHOBIT PRASHANT NAIR', '2019csb1121@iitrpr.ac.in', 'JAO');
-INSERT INTO public.users (name, email, role) VALUES ('PUNEET GOYAL', 'puneet@iitrpr.ac.in', 'JAO');
-INSERT INTO public.users (name, email, role) VALUES ('Shobit Nair', 'shobitnair@gmail.com', 'FACULTY');
+INSERT INTO public.users (name, email, role, department) VALUES ('Sujaya prashant', 'sujaprasha1995@gmail.com', 'FACULTY', null);
+INSERT INTO public.users (name, email, role, department) VALUES ('PUNEET GOYAL', 'puneet@iitrpr.ac.in', 'JAO', null);
+INSERT INTO public.users (name, email, role, department) VALUES ('Shobit Nair', 'shobitnair@gmail.com', 'FACULTY', 'CSE');
+INSERT INTO public.users (name, email, role, department) VALUES ('SHOBIT PRASHANT NAIR', '2019csb1121@iitrpr.ac.in', 'JAO', 'ACCOUNTS');
+INSERT INTO public.users (name, email, role, department) VALUES ('Shobit Nair', 'shobitnair10@gmail.com', 'FACULTY', 'CSE');
